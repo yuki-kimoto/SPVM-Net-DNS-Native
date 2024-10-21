55dc76333602c4355d053eebee33b4cf9c459778
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-10-21 14:15:37 +0900

    Initial commit.

---
+++ lib/SPVM/Net/DNS/Native.pm
@@ -0,0 +1,65 @@
+package SPVM::Net::DNS::Native;
+
+our $VERSION = "0.001";
+
+1;
+
+=head1 Name
+
+SPVM::Net::DNS::Native - Short Description
+
+=head1 Description
+
+Net::DNS::Native class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Net::DNS::Native;
+
+=head1 Details
+
+
+
+=head1 Super Class
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 See Also
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
